54902f4759fd1f559e65d54eebedf7b41ad8187e
William Newell <newellwm@gmail.com>
2021-09-15 22:56:49 -0500

    Stub additional methods and clean up code

--- src/ip_math.c
+++ src/ip_math.c
@@ -4,29 +4,23 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <math.h>
 
-void print_help();
-void print_commands();
-void call_library_method(const char **argv);
-void print_broadcast_address(const char *ip_address, const char *mask);
-void print_integer_ip(const char *ip_address);
+static void print_help();
+static void print_commands();
+static void call_library_method(const char **argv);
+static void print_broadcast_address(const char *ip_address, const char *mask);
+static void print_network_id(const char *ip_address, const char *mask);
+static void print_integer_ip(const char *ip_address);
+static void print_dot_ip(const char *ip_address);
+static void print_is_in_subnet(const char *network_id, const char *mask, const char *ip_address);
+static void print_subnet_cardinality(uint8_t mask);
 
 void get_broadcast_address(const char *ip_address, uint8_t mask, char *output_buffer) {
-
   uint_fast32_t integer_ip = get_ip_integer_equivalent(ip_address);
   uint_fast32_t bit_mask = UINT32_MAX >> mask;
   uint_fast32_t integer_broadcast = integer_ip | bit_mask;
-
-  for (int i = 24; i >= 0; i-=8) {
-    const int bytes_written = sprintf(output_buffer, "%hhu", (uint8_t)(integer_broadcast >> i));
-    output_buffer += bytes_written;
-
-    if (i > 0) {
-      *output_buffer = '.';
-      output_buffer ++;
-    }
-  }
-	return;
+  get_dot_ip_format(integer_broadcast, output_buffer);
 }
 
 uint_fast32_t get_ip_integer_equivalent(const char *ip_address) {
@@ -47,6 +41,34 @@ uint_fast32_t get_ip_integer_equivalent(const char *ip_address) {
 	return integer_ip;
 }
 
+// TOTEST (explicitly)
+void get_dot_ip_format(uint_fast32_t ip_address, char *output_buffer) {
+  for (int i = 24; i >= 0; i-=8) {
+    const int bytes_written = sprintf(output_buffer, "%hhu", (uint8_t)(ip_address >> i));
+    output_buffer += bytes_written;
+
+    if (i > 0) {
+      *output_buffer = '.';
+      output_buffer ++;
+    }
+  }
+}
+
+// TOTEST
+uint_fast32_t get_subnet_cardinality(uint8_t mask) {
+  return pow(2, mask) - 2;
+}
+
+// TODO
+bool is_ip_in_subnet(char *network_id, uint8_t mask, char* ip_address) {
+  return 0;
+}
+
+void get_network_id(char *ip_address, uint8_t mask, char *output_buffer) {
+  uint_fast32_t integer_ip = get_ip_integer_equivalent(ip_address);
+  uint_fast32_t integer_network_id = (integer_ip >> mask) << mask;
+  get_dot_ip_format(integer_network_id, output_buffer);
+}
 
 int main(int argc, const char **argv) {
   if (argc == 2) {
@@ -60,27 +82,36 @@ int main(int argc, const char **argv) {
   return 0;
 }
 
-void print_help() {
+// TODO update with new commands
+static void print_help() {
   printf("Examples of each command are as follows: \n");
   printf("broadcast 192.168.1.1 24\n");
   printf("integer 192.168.1.1\n");
 }
 
-void print_commands() {
+static void print_commands() {
     printf("Possible commands are: broadcast, integer, help\n");
 }
 
-void call_library_method(const char **argv) {
+static void call_library_method(const char **argv) {
   const char *method_name = argv[1];
-  if (strcmp(method_name, "broadcast") == 0)
+  if(strcmp(method_name, "in_subnet") == 0)
+    print_is_in_subnet(argv[2], argv[3], argv[4]);
+  else if (strcmp(method_name, "broadcast") == 0)
     print_broadcast_address(argv[2], argv[3]);
+  else if(strcmp(method_name, "network") == 0)
+    print_network_id(argv[2], argv[3]);
   else if(strcmp(method_name, "integer") == 0)
     print_integer_ip(argv[2]);
+  else if(strcmp(method_name, "dot") == 0)
+    print_dot_ip(argv[2]);
+  else if(strcmp(method_name, "hosts") == 0)
+    print_dot_ip(argv[2]);
   else
     print_commands();
 }
 
-void print_broadcast_address(const char *ip_address, const char *mask) {
+static void print_broadcast_address(const char *ip_address, const char *mask) {
     char output[16] = {};
     uint8_t broadcast = strtol(mask, NULL, 10);
 
@@ -88,6 +119,26 @@ void print_broadcast_address(const char *ip_address, const char *mask) {
     printf("broadcast address: %s\n", output);
 }
 
-void print_integer_ip(const char *ip_address) {
+static void print_integer_ip(const char *ip_address) {
   printf("integer ip address: %lu\n", get_ip_integer_equivalent(ip_address));
+}
+
+// TODO
+static void print_subnet_cardinality(uint8_t mask) {
+
+}
+
+// TODO
+static void print_network_id(const char *ip_address, const char *mask) {
+
+}
+
+// TODO
+static void print_dot_ip(const char *ip_address) {
+
+}
+
+// TODO
+static void print_is_in_subnet(const char *network_id, const char *mask, const char *ip_address) {
+
 }
--- src/ip_math.h
+++ src/ip_math.h
@@ -1,4 +1,6 @@
 #include <inttypes.h>
+#include <stdbool.h>
+
 /*
  * USAGE:
  * char ipadddress_buffer[PREFIX_LENGTH]; // (16)
@@ -21,3 +23,49 @@ void get_broadcast_address(const char *ip_address, uint8_t mask, char *output_bu
  * EX: 10.1.23.10 -> 167_843_594
  */
 uint_fast32_t get_ip_integer_equivalent(const char *ip_address);
+
+/*
+ * USAGE:
+ * unsigned int int_ip = 2058138165;
+ * char ip_address_buffer[PREFIX_LENGTH];
+ * memset(ip_address_buffer, 0, PREFIX_LENGTH);
+ * get_dot_ip_format(int_ip, ip_address_buffer);
+ * printf("IP address in A.B.C.D format is %s\n", ip_address_buffer);
+ * EX: 2058138165 -> 122.172.178.53
+ */
+void get_dot_ip_format(uint_fast32_t ip_address, char *output_buffer);
+
+/*
+ * USAGE:
+ * char network_id_buffer[PREFIX_LENGTH];
+ * memset(ip_address_buffer, 0, PREFIX_LENGTH);
+ * char *ip_address = "192.168.2.10";
+ * uint8_t mask = 20;
+ * get_network_id(ip_address, mask, network_id_buffer);
+ * printf("Network id is %s\n");
+ * EX: 192.168.1.1 24 -> 129.168.0.0
+ * EX: 10.1.23.10 20 -> 10.1.16.0
+ */
+void get_network_id(char *ip_address, uint8_t mask, char *output_buffer);
+
+/*
+ * USAGE:
+ * unsigned char mask = 24;
+ * printf("Subnet cardinality for mask = %u is %u\n", mask, get_subnet_cardinality(mask));
+ * EX: 24 -> 254
+ * EX: 30 -> 2
+ */
+void get_subnet_cardinality(uint8_t mask);
+
+/*
+ * USAGE:
+ * char *network_id = "192.168.0.0";
+ * char mask = 24;
+ * char *check_ip = "192.168.0.13"
+ * bool is_in_subnet = is_ip_in_subnet(network_id, mask, check_ip);
+ * char* conditional = (is_in_subnet) ? "" : " not";
+ * printf("IP address %s is%s a member of subnet %s/%u\n", check_ip, conditional, network_id, mask);
+ * EX: 192.168.0.0 24 192.168.0.13 -> true
+ * EX: 192.168.0.0 24 192.169.0.13 -> false
+ */
+bool is_ip_in_subnet(char *network_id, uint8_t mask, char* ip_address);
